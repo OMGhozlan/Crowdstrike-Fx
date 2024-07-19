decc7e3677d09010a00e218cef73cf3a647a613b
Osama Ghozlan <osama.ghozlan@pwc.com>
2024-07-19 13:13:46 +0300

    Initial commit

---
+++ CSFix.ps1
@@ -0,0 +1,47 @@
+Write-Output "======================"
+Write-Output "CrowdStrike Fix by OG"
+Write-Output "======================"
+
+$drive = $env:SystemDrive
+$directoryPath = "$($drive)\Windows\System32\drivers\CrowdStrike"
+$filePattern = "C-00000291*.sys"
+$logFilePath = "$($drive)\Temp\script_log.txt"
+
+# Check if the Temp directory exists, create it if not
+if (-Not (Test-Path "$($drive)\Temp")) {
+    New-Item -ItemType Directory -Path "$($drive)\Temp" | Out-Null
+}
+
+try {
+    "[$(Get-Date)] Script started." | Out-File -Append -FilePath $logFilePath
+
+    Set-Location -Path $directoryPath
+
+    # Get the list of files matching the pattern
+    $matchingFiles = Get-ChildItem -Filter $filePattern
+
+    # Log matching files to the log file and print to output
+    if ($matchingFiles.Count -eq 0) {
+        "[$(Get-Date)] No matching files found." | Out-File -Append -FilePath $logFilePath
+        Write-Output "No matching files found."
+    } else {
+        $matchingFiles | ForEach-Object {
+            $fileName = $_.Name
+            "[$(Get-Date)] Found matching file: $fileName" | Out-File -Append -FilePath $logFilePath
+            Write-Output "Found matching file: $fileName"
+        }
+
+        # Delete the matching files
+        $matchingFiles | ForEach-Object {
+            $fileName = $_.Name
+            Remove-Item -Path $_.FullName -Force
+            "[$(Get-Date)] Deleted file: $fileName" | Out-File -Append -FilePath $logFilePath
+            Write-Output "Deleted file: $fileName"
+        }
+    }
+
+    Write-Output "Script completed successfully. Log file saved at $logFilePath"
+} catch {
+    Write-Output "Error occurred: $_"
+    Write-Output "Please ensure that your computer is in Safe Mode and rerun the script."
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Crowdstrike-Fx
+# CrowdStrike Fix Script
+
+## Description
+A simple PowerShell script for the CrowdStrike issue of June 2024. It performs the following actions:
+1. Locate and delete files matching the pattern "C-00000291*.sys" in the CrowdStrike directory.
+
+## Usage
+1. Save the script to a convenient location on your system.
+2. Open PowerShell as an administrator.
+3. Run the script using the following command:
+   ```powershell
+   .\CrowdStrikeFix.ps1
+   ```
+4. Follow any prompts or instructions provided by the script.
+5. 
+
+## Notes
+- Ensure that you have appropriate permissions to modify files and reboot the system.
+- If the script fails due to permissions, try running it as admin or an elevated user
+- The script will create a log file at `C:\Temp\script_log.txt`.
+- If no matching files are found, the script will indicate so in the log and output.
